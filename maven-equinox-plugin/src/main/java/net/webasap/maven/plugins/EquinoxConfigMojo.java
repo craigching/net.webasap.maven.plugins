ab574659c969158fd97b966c975d8fb93ba802eb
Craig L. Ching <craigching@gmail.com>
2009-11-09 21:08:04 -0600

    * Added copyright statements

--- maven-equinox-plugin/src/main/java/net/webasap/maven/plugins/EquinoxConfigMojo.java
+++ maven-equinox-plugin/src/main/java/net/webasap/maven/plugins/EquinoxConfigMojo.java
@@ -1,7 +1,7 @@
 package net.webasap.maven.plugins;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Copyright 2009 Craig L. Ching
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
