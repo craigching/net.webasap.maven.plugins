a547377cff0399ff712210cde413a01664adff0f
Craig L. Ching <craigching@gmail.com>
2009-11-10 21:20:57 -0600

    * Cleaned up code a bit, added some comments to important sections in the generated config.ini

--- maven-equinox-plugin/src/main/java/net/webasap/maven/plugins/EquinoxConfigMojo.java
+++ maven-equinox-plugin/src/main/java/net/webasap/maven/plugins/EquinoxConfigMojo.java
@@ -24,10 +24,15 @@ import org.apache.maven.project.MavenProject;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.Enumeration;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 /**
  * Goal which touches a timestamp file.
@@ -41,6 +46,7 @@ public class EquinoxConfigMojo
     extends AbstractMojo
 {
 	final static private String LINE_SEP = System.getProperty("line.separator");
+	final static private String SYSTEM_PKGS = "org.osgi.framework.system.packages";
     /**
      * @parameter default-value="${project}"
      * @required
@@ -70,10 +76,7 @@ public class EquinoxConfigMojo
      */
     private List<BundleGroup> bundleGroups;
     
-    public void execute()
-        throws MojoExecutionException
-    {
-    	Set<Artifact> projectArtifacts = project.getArtifacts();
+    private void resolveBundles(Set<Artifact> artifacts) {
     	for (BundleGroup bg : this.bundleGroups) {
     		getLog().info("====== Bundle Group: Start Level: " + bg.getStartLevel());
     		String [] includes = bg.getIncludes();
@@ -83,7 +86,7 @@ public class EquinoxConfigMojo
     			String groupId = s[0];
     			String artifactId = s[1];
     			Artifact found = null;
-    			for (Artifact a : projectArtifacts) {
+    			for (Artifact a : artifacts) {
     				if (a.getArtifactId().equals(artifactId) && a.getGroupId().equals(groupId)) {
     					found = a;
     					break;
@@ -91,7 +94,7 @@ public class EquinoxConfigMojo
     			}
     			
     			if (found != null) {
-    				projectArtifacts.remove(found);
+    				artifacts.remove(found);
     				bg.addArtifact(found);
     			} else {
     				getLog().error("No such artifact in project dependencies: " + i);
@@ -99,7 +102,34 @@ public class EquinoxConfigMojo
     		}
     		getLog().info("=================");
     	}
+    }
+    
+    private String getBundleReference(File f, int sl) {
+    	StringBuilder buf = new StringBuilder();
+		buf.append("reference:file:")
+		.append(bundlesDir)
+		.append("/")
+		.append(f.getName());
+		
+		try {
+			if (!isFragment(f)) {
+				buf.append("@");
+				if (sl > 0) {
+					buf.append(sl).append(":");
+				}
+				buf.append("start");
+			}
+		} catch (IOException e) {
+		}
+		return buf.toString();
+    }
+    
+    public void execute()
+        throws MojoExecutionException
+    {
 
+    	Set<Artifact> projectArtifacts = project.getArtifacts();
+    	resolveBundles(projectArtifacts);
         File f = outputDirectory;
 
         if ( !f.exists() )
@@ -107,36 +137,58 @@ public class EquinoxConfigMojo
             f.mkdirs();
         }
 
-        File touch = new File( f, "config.ini" );
-
-        FileWriter w = null;
+        File configFile = new File( f, "config.ini" );
+        
+        Writer w = null;
         try
         {
-            w = new FileWriter( touch );
+            w = new FileWriter( configFile );
+            
+            StringBuilder buf = new StringBuilder();
+            List<String> bundleRefs = new LinkedList<String>();
 
         	Enumeration<?> p = properties.propertyNames();
         	while(p.hasMoreElements()) {
         		String k = (String)p.nextElement();
         		String v = properties.getProperty(k);
-        		w.write(k + "=" + v + LINE_SEP);
+        		// Ignore the system packages property for now
+        		// we want to put it in its own section
+        		if (!k.equals(SYSTEM_PKGS)) {
+        			buf.append(k).append("=").append(v).append(LINE_SEP);
+        		}
         	}
-        	w.write("osgi.bundles=\\" + LINE_SEP);
+        	
+        	// Handle the system packages now
+        	buf.append(LINE_SEP).append("### System Packages ###").append(LINE_SEP);
+        	buf.append(SYSTEM_PKGS).append("=").append(properties.getProperty(SYSTEM_PKGS)).append(LINE_SEP);
+        	
+        	buf.append(LINE_SEP).append("### Bundles ###").append(LINE_SEP);
+        	buf.append("osgi.bundles=\\").append(LINE_SEP);
         	getLog().info(projectArtifacts.size() + " artifacts.");
         	for (BundleGroup bg : bundleGroups) {
         		int startLevel = bg.getStartLevel();
         		List<Artifact> artifacts = bg.getArtifacts();
         		for (Artifact a : artifacts) {
-            		w.write("reference:file:" + bundlesDir + "/" + a.getFile().getName() + "@" + startLevel + ":start,\\" + LINE_SEP);
+        			bundleRefs.add(getBundleReference(a.getFile(), startLevel));
         		}
         	}
         	for (Artifact a : projectArtifacts) {
-        		// TODO If a bundle is a fragment, don't start it
-        		w.write("reference:file:" + bundlesDir + "/" + a.getFile().getName() + "@start,\\" + LINE_SEP);
+        		bundleRefs.add(getBundleReference(a.getFile(), -1));
         	}
+        	
+        	int num_refs = bundleRefs.size();
+        	for (String ref : bundleRefs) {
+        		buf.append(ref);
+        		if (--num_refs > 0) {
+        			buf.append(",\\" + LINE_SEP);
+        		}
+        	}
+        	
+        	w.write(buf.toString());
         }
         catch ( IOException e )
         {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
+            throw new MojoExecutionException( "Error creating file " + configFile, e );
         }
         finally
         {
@@ -152,5 +204,16 @@ public class EquinoxConfigMojo
                 }
             }
         }
+    }
+
+    final static private boolean isFragment(File f) throws IOException {
+    	boolean isFragment = false;
+    	JarFile j = new JarFile(f);
+    	Manifest m = j.getManifest();
+    	if (m != null) {
+    		Attributes attrs = m.getMainAttributes();
+    		isFragment = attrs.containsKey("Fragment-Host");
+    	}
+    	return isFragment;
     }
 }
